6cbe2e52608d4b9199aecd7645b8cd134349f9e0
mark.sancho <mark.sancho@NDPHPBI7-MSANCH>
2017-06-27 12:51:58 +0800

    updated bot to speak 2

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 
+const token = 'EAATuSDbZCxEcBAJrh7ZCgnmitZAQLEZBhxEEIeFi9D1LGo325QETkgGlmkuJ0BTyNPFnZAth3bZBmSIHNk8jHv1iika1tDwZB1f76yY6mx7QalkcjaYG2Nbi48cJN7s3ZBsMp49EokyZCa88ZCSyib2mfDBZCvZBZAU2anKbIDFLFNAqUwAZDZD';
+
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -23,7 +25,39 @@ app.get('/webhook/', function (req, res) {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
-})
+});
+
+app.post('/webhook/', function (req, res) {
+    var messaging_events = req.body.entry[0].messaging
+    for (var i = 0; i < messaging_events.length; i++) {
+        var event = req.body.entry[0].messaging[i]
+        var sender = event.sender.id
+        if (event.message && event.message.text) {
+            var text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+});
+
+function sendTextMessage(sender, text) {
+    var messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 
 // Spin up the server
 app.listen(app.get('port'), function() {
