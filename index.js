cbead9cc6917e7f36cffe86daf3b69a76ee5570c
mark.sancho <mark.sancho@NDPHPBI7-MSANCH>
2017-06-27 10:35:01 +0800

    hello world

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * Created by mark.sancho on 6/27/2017.
+ */
+'user strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.get('/', function (req, res) {
+    res.send('Hello world, I am a chat bot');
+});
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+
+
